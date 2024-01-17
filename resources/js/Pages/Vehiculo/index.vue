6e3f58cb00ef879af30c17f8ad560b990ec7d57b
apolanco <apolanco@cdevteam.com>
2024-01-17 16:51:38 -0400

    optimizando

--- resources/js/Pages/Vehiculo/index.vue
+++ resources/js/Pages/Vehiculo/index.vue
@@ -473,9 +473,6 @@ export default {
                     let marca = this.filteredMarcas[0]
                     this.selectMarca(marca)
                 }
-                else{
-                    this.form.marca = ""
-                }
                 this.showDropdown = false;
             }, 200);
         },
