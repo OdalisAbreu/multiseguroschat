e63c1c2122e288bcc03e4637f0b97b4d569bc097
apolanco <apolanco@cdevteam.com>
2024-02-02 16:22:10 -0400

    custom components

---
+++ resources/js/components/CustomButton.vue
@@ -0,0 +1,29 @@
+<template>
+    <div
+        class="w-full mt-5 mx-5 my-4 justify-self-center self-center text-center"
+    >
+        <button
+            :id="id"
+            :disabled="disabled"
+            class="w-full max-w-xl justify-center bg-blue-800 hover:bg-blue-700 shadow-lg shadow-blue-500/50 text-white font-bold rounded-lg py-4 mt-5 sm:m-3 sm:w-full md:m-3 md:w-full xl:m-3 xl:full"
+        >
+            {{Text}}
+        </button>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"CustomButton",
+    props:{
+        id: String,
+        Text:{
+            type:String,
+            default:"Button"
+        },
+        disabled:Boolean,
+    }
+};
+</script>
+
+<style></style>
---
+++ resources/js/components/CustomInput.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="w-full flex flex-col lg:items-center mt-1">
+        <label class="pt-1 font-bold w-auto">
+            {{ labelName }}
+            <span v-if="isObligatory" class="text-red-400 inl">* {{ value }}</span>
+        </label>
+        <input
+            class="rounded-lg w-full border-gray-300"
+            :class="{'invalid': error}"
+            :id="id"
+            :labelName="labelName"
+            :value="modelValue"
+            :type="typeInput"
+            :placeholder="placeholder"
+            :disabled="disabled"
+            @input="$emit('input', $event.target.value)"
+        />
+    </div>
+    <div class="relative w-full">
+        <span v-if="error" class="text-red-500 float-left">{{
+            errorMessage
+        }}</span>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: "CustomInput",
+    props: {
+        id: String,
+        labelName: {
+            type: String,
+            required: true,
+            default: "Label",
+        },
+        modelValue: { type: String, required: true },
+        isObligatory: { type: Boolean, default: false,},
+        typeInput: { type: String, default: "text"},
+        placeholder: {type: String, default: ""},
+        errorMessage:String,
+        error:Boolean,
+        disabled:Boolean
+
+    },
+    data() {
+        return {};
+    },
+};
+</script>
+
+<style>
+.invalid {
+    background-color: pink;
+    border: solid 1px red;
+}
+</style>
